0cb8407b51ff8bf72b539f5222934e133cf41aee
revolexGeek <windowsguide1337@gmail.com>
2024-08-07 14:41:58 +0200

    Feature: Implemented authorization

--- src/frontend/nuxt.config.ts
+++ src/frontend/nuxt.config.ts
@@ -19,6 +19,28 @@ export default defineNuxtConfig({
     download: true,
     base64: false,
   },
-  modules: ["@nuxt/ui", "@nuxtjs/google-fonts"],
+  auth: {
+    isEnabled: true,
+    disableServerSideAuth: false,
+    baseURL: "http://127.0.0.1:8000/api/auth/",
+    provider: {
+      type: "local",
+      token: {
+        signInResponseTokenPointer: "/key",
+        type: "Token",
+        cookieName: "sessionid",
+        headerName: "Authorization",
+        maxAgeInSeconds: 1800,
+        sameSiteAttribute: "lax",
+      },
+      endpoints: {
+        signIn: { path: "login/", method: "post" },
+        signOut: { path: "logout/", method: "post" },
+        signUp: { path: "register/", method: "post" },
+        getSession: { path: "user/", method: "get" },
+      },
+    },
+  },
+  modules: ["@nuxt/ui", "@nuxtjs/google-fonts", "@sidebase/nuxt-auth"],
   compatibilityDate: "2024-08-05",
 });
